162aab09aed727641ef9fdf8b35bd207ca103d78
Oliver Hulett <oliver.hulett@gmail.com>
2018-03-04 00:46:06 +1100

    Add some test code, ignore some files

--- tests/cloc/test_classification_description.py
+++ tests/cloc/test_classification_description.py
@@ -5,20 +5,47 @@ Created on 3 Mar. 2018
 '''
 import unittest
 
+from cloc.classification_description import ClassificationDescription
+
 
 class TestClassificationDescription(unittest.TestCase):
 
 
     def setUp(self):
-        pass
-
+        self.description = ClassificationDescription()
+        self.python_description_1 = {
+            "comment": {
+                "path_regex": '\\.py$',
+                "line_regex": '^\\s*#',
+                "classifications": ["Comments"],
+            },
+            "docstring_singleline": {
+                "path_regex": '\\.py$',
+                "line_regex": '^\\s*(\'\'\'|""").*\\1\\s*$',
+                "classifications": ["Comments"],
+            },
+            "docstring_multiline": {
+                "path_regex": '\\.py$',
+                "entry_regex": '^\\s*(\'\'\'|""")',
+                "exit_regex": '\\1\\s*$',
+                "classifications": ["Comments"],
+            },
+        }
 
     def tearDown(self):
         pass
 
+    def test_add_description_good(self):
+        try:
+            self.description.add_descriptions(self.python_description_1)
+        except Exception as e:
+            pytest.fail("Unexpected exception raised: " + str(e))
 
-    def testName(self):
-        pass
+    def test_add_description_multiple(self):
+        try:
+            self.description.add_descriptions(self.python_description_1)
+        except Exception as e:
+            pytest.fail("Unexpected exception raised: " + str(e))
 
 
 if __name__ == "__main__":
