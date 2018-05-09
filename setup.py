eb9f2d005499f19dc9abc51e92682ce5a1170fa2
Oliver Hulett <ohulett@atlassian.com>
2018-05-09 12:01:18 +1000

    Fix setup.py for develop install and normal install

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+
+
+def get_package_data_files():
+    files = []
+    pkgdir = os.path.join(os.path.dirname(__file__), "src", "cloc")
+    for root, _, filenames in os.walk(os.path.join(pkgdir, "config")):
+        for f in filenames:
+            files.append(os.path.join(root[len(pkgdir) + 1:], f))  # plus 1 for the slash
+    return files
+
+
+print get_package_data_files()
 
 setup(
     name='cloc',
@@ -11,7 +24,7 @@ setup(
     description='Classify Lines Of Code',
     package_dir={'': "src"},
     packages=['cloc'],
-    package_data={'cloc': ["cloc/config/*"]},
+    package_data={'cloc': get_package_data_files()},
     entry_points={'console_scripts': ['cloc=cloc.__main__:main']},
     install_requires=[
         'pyyaml',
